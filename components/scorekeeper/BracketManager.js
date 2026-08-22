e614333cafc449d659e51ecca33be09c1b61016d
JD WILLCOX <jdwillcox@JDs-MacBook-Pro.local>
2026-08-22 08:59:22 -0600

    Hide Photo of sheet until we want that path
    The read-from-photo code stays in the repo; it is just not on the
    director page.

--- components/scorekeeper/BracketManager.js
+++ components/scorekeeper/BracketManager.js
@@ -12,7 +12,6 @@ import { isSurvivorPoolGame } from "@/lib/bracket/lives";
 import { formatGameWhenInput, parseGameWhenInput } from "@/lib/league-time";
 import GameWhenInput from "./GameWhenInput";
 import { directorPost } from "./DirectorForm";
-import ReadSheet from "./ReadSheet";
 
 const FORMATS = [
   { value: "three_gg_hybrid", label: "3GG" },
@@ -180,14 +179,6 @@ export default function BracketManager({
       )}
 
       {/* 4. Score */}
-      {hasBracket && (
-        <ReadSheet
-          divisionId={divisionId}
-          playDay={playDay}
-          teamNames={teams}
-          onApplied={() => router.refresh()}
-        />
-      )}
       {hasBracket && mainGames.length > 0 && (
         <div className="space-y-4">
           <ScoreList
